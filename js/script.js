8321db82559eec9b7b88e82e22a4e7b5d0fc846f
Eva Kiss <eva.vera.kiss@gmail.com>
2019-10-29 21:08:05 +1100

    Yay i hope it works

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('YAY it loaded');
 
+
+if ($('#map').length){
 // Map section
 
 	mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhciIsImEiOiJjazFwZ3ZqZ24wOXlkM2NtdTk1ZDg2dDRpIn0.MLFm1hFFjqz5mJpNy3AvWg';
@@ -8,36 +10,17 @@ console.log('YAY it loaded');
 		container: 'map', // container id
 		style: 'mapbox://styles/evar/ck21kmpkw56i61ckif2z156zh', //hosted style id
 		center: [151.252, -33.826], // starting position
-		zoom: 14.1 // starting zoom
+		zoom: 14.0 // starting zoom
 	
 	});
 
 
-// This is your link to what you created online
-// map.on('click', function(e) {
-//   var features = map.queryRenderedFeatures(e.point, {
-//     layers: ['badbutts-balmoral-final'] // replace this with the name of the layer
-//   });
-
-//   if (!features.length) {
-//     return;
-//   }
-
-//   var feature = features[0];
-
-//   var popup = new mapboxgl.Popup({ offset: [0, -15] })
-//     .setLngLat(feature.geometry.coordinates)
-//     .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-//     .addTo(map);
-// });
-
-
 map.on('load', function() {
  
- // custom marker here
-map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function(error, image) {
+ // Custom marker here
+map.loadImage('https://i.ibb.co/zN6RPbX/maps-and-flags.png', function(error, image) {
 if (error) throw error;
-map.addImage('cat', image);
+map.addImage('pin', image);
 
 	// Add a layer showing the places.
 	map.addLayer({
@@ -53,7 +36,7 @@ map.addImage('cat', image);
 					"type": "Feature",
 					"properties": {
 						"description": 
-						"<h4>Clean Up #1</h4><p><i>20th October, 2019</i> <br><br> Items found: <ul><li>350 cigarette butts </li> <li> 12 straws </li> <li> 36 metal bottle caps </li></ul></p>",
+						"<h4>Clean Up #1</h4><p><i>19th October, 2019 - 12 people</i> <br><br> Items found: <ul class='popuplist'><li>161 cigarette butts </li> <li> 360+ soft plastics </li> <li> 58 metal bottle caps </li><li> 17 plastic lids </li> </ul> <a href='#'>read the comprehensive list</a> <br><a href='https://www.facebook.com/photo.php?fbid=10162677748735553&set=pcb.2546814802074218&type=3&theater&ifg=1'>see our photos </a></p>",
 					},
 
 					"geometry": {
@@ -65,62 +48,89 @@ map.addImage('cat', image);
 					"type": "Feature",
 					"properties": {
 						"description": 
-						"<h4>Clean Up #2</h4><p>Items found: <ul><li>350 cigarette butts </li> <li> 12 straws </li> <li> 36 metal bottle caps </li></ul></p>",
+						"<h4>Clean Up #2</h4><p><i>27th October, 2019 - 16 people</i> <br><br>Items found: <ul class='popuplist'><li>202 cigarette butts </li> <li> 12 straws </li> <li> 36 metal bottle caps </li><li>12 broken glass</li></ul><a href='#'>read the comprehensive list</a> <br><a href='https://www.facebook.com/photo.php?fbid=10162677748735553&set=pcb.2546814802074218&type=3&theater&ifg=1'>see our photos </a></p>",
 					},
 					"geometry": {
 					"type": "Point",
-					"coordinates": [151.25357949360148, -33.82703028980452]
+					"coordinates": [151.252861, -33.826962]
 					}
+				
+				}, {
+					"type": "Feature",
+					"properties": {
+						"description": 
+						"<h4>Clean Up #3</h4><p><i>9th November, 2019 - 5 people</i> <br><br>Items found: <ul class='popuplist'><li>86 cigarette butts </li> <li> 308 soft plastics</li><li> 2 straws </li> <li> 11 metal bottle caps </li></ul><a href='#'>read the comprehensive list</a> <br><a href='https://www.facebook.com/photo.php?fbid=10162677748735553&set=pcb.2546814802074218&type=3&theater&ifg=1'>see our photos </a></p>",
+					},
+					"geometry": {
+					"type": "Point",
+					"coordinates": [151.25116500000001, -33.8241425]
+					}
+
+				}, {
+					"type": "Feature",
+					"properties": {
+						"description": 
+						"<h4>Clean Up #4</h4><p><i>8th January, 2020 - 15 people</i> <br><br>Items found: <ul class='popuplist'><li>385 cigarette butts </li> <li> 23 straws </li> <li> 67 metal bottle caps </li><li> 21 salt packets</li></li></ul><a href='#'>read the comprehensive list</a> <br><a href='https://www.facebook.com/photo.php?fbid=10162677748735553&set=pcb.2546814802074218&type=3&theater&ifg=1'>see our photos </a></p>",
+					},
+					"geometry": {
+					"type": "Point",
+					"coordinates": [151.251171, -33.823202]
+					}
+
 
 				}]
 			}
 		},
 
 		"layout": {
-			"icon-image": "cat",
-			"icon-size": 0.1,
+			"icon-image": "pin",
+			"icon-size": 0.26,
 			"icon-allow-overlap": true,
 		}
 
 	});
 });
 
-	var popup = new mapboxgl.Popup({
-		closeButton: false,
-		closeOnClick: false
-	});
+map.scrollZoom.disable();
+
+map.addControl(new mapboxgl.NavigationControl());
+
+var popup = new mapboxgl.Popup({
+	closeButton: true,
+	closeOnClick: true
+});
 
 
-	map.on('mouseenter', 'places', function(e) {
-		// Change the cursor style as a UI indicator.
-		map.getCanvas().style.cursor = 'pointer';
+map.on('mouseenter', 'places', function(e) {
+	map.getCanvas().style.cursor = 'pointer';
 
 
-		var coordinates = e.features[0].geometry.coordinates.slice();
-		var description = e.features[0].properties.description;
+	var coordinates = e.features[0].geometry.coordinates.slice();
+	var description = e.features[0].properties.description;
 
 
-		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-		}
+	while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+		coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+	}
 
-		popup.setLngLat(coordinates)
-			.setHTML(description)
-			.addTo(map);
+	popup.setLngLat(coordinates)
+		.setHTML(description)
+		.addTo(map);
 	});
  
  	map.on('mouseleave', 'places', function() {
 		map.getCanvas().style.cursor = '';
-		popup.remove();
 	});
 
 
 });
 
-
+}
 
 
 // Floaties section
+
+if ($('.floaties').length){
 
 var cigbutt = '../personal_project/img/singlebutt.png';
 
@@ -149,11 +159,15 @@ cigbutts.forEach( function(e, i, a) {
   })
 });
 
+}
+
 
 // Carousel section
 
+if ($('.hero-carousel').length){
+
 $('.hero-carousel').slick({ 
-	autoplay      : true,
+	autoplay      : false,
 	autoplaySpeed : 3000,
 	dots          : true, 
 	prevArrow     : '<i class="fa fa-chevron-left"></i>',
@@ -162,3 +176,10 @@ $('.hero-carousel').slick({
 	slidesToShow  : 2,
 	slidesToScroll : 1,
 });
+
+}
+
+
+
+
+
